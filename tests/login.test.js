acb8417dda821fd35dfc8e8c00141c69f7d11b65
George Mathias <gmathias80@gmail.com>
2026-06-24 07:55:09 +0100

    configurando VUs e duration para a execução dos testes

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,13 +2,13 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-    iterations: 20,
+    vus: 20,
+    duration: '30s',
     thresholds: {
         http_req_failed: ['rate < 0.01'],
         http_req_duration: [
-            'p(90) < 1.70', 
-            'p(95) < 1.60', 
-            'max < 1'
+            'p(90) < 3000',  
+            'max < 5000'
         ]
     }
 }
