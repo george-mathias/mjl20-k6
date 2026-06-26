3dde246e64d30f68078294d6bcf2fbe613099334
George Mathias <gmathias80@gmail.com>
2026-06-26 09:03:51 +0100

    adicionando stages, thresholds e fixture de transferencias

--- tests/transferencia.test.js
+++ tests/transferencia.test.js
@@ -4,18 +4,25 @@ import { obterToken } from '../helpers/autenticacao.js'
 const transferencia = JSON.parse(open("../fixtures/postTransferencias.json"));
 
 export const options = {
-    iterations: 1
+    // iterations: 1,
+    stages: [
+        { duration: '5s', target: 10 },
+        { duration: '5s', target: 10 },
+        { duration: '5s', target: 0 }
+    ],
+    thresholds: {
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: [
+            'p(90) < 3000',
+            'max < 5000'
+        ]
+    }
 };
 
 export default function () {
     const url = 'http://localhost:3000/transferencias';
     const token = obterToken();
-    const payload = JSON.stringify({
-        "contaOrigem": 1,
-        "contaDestino": 2,
-        "valor": 10,
-        "token": ""
-    })
+    const payload = JSON.stringify(transferencia)
     const params = {
         headers: {
             "Content-Type": "application/json",
